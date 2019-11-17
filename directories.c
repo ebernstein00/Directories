e67b07cc9fb9ac1ef665857ca802ec5cde0b3be2
Elias Bernstein <41696293+ebernstein00@users.noreply.github.com>
2019-11-17 18:12:53 -0500

    Update directories.c

--- directories.c
+++ directories.c
@@ -53,14 +53,14 @@ int main(){
   //int j;
   for (i = 0; i < 10; i++){
     //for (j = 0; j < 250; j++){
-    if (dirs[i] == "\0") break;
+    if (dirs[i] == NULL) break;
     printf("%s\n", dirs[i]);
     //}
   };
   printf("\nFiles:\n");
   for (i = 0; i < 10; i++){
     //for (j = 0; j < 250; j++){
-    if (files[i] == "\0") break;
+    if (files[i] == NULL) break;
     printf("%s\n", files[i]);
     //}
   };
