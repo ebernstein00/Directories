fafb27b2a7d187d8e1f800455b850fccbe95ce12
Elias Bernstein <41696293+ebernstein00@users.noreply.github.com>
2019-11-17 17:44:55 -0500

    Delete filefuncs.h

--- filefuncs.h
+++
@@ -1,4 +0,0 @@
-void print_files(struct dir* node);
-struct dir* insert_file(struct dir* node, char x[250]);
-struct dir* free_files(struct dir* node);
-struct dir* remove_file(struct dir* front, char data[250]);
