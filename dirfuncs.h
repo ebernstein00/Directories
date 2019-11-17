24d48b7e2bfb0ac3e8c66b27e98448e5bb77fa64
Elias Bernstein <41696293+ebernstein00@users.noreply.github.com>
2019-11-17 17:44:47 -0500

    Delete dirfuncs.h

--- dirfuncs.h
+++
@@ -1,4 +0,0 @@
-void print_dirs(struct dir* node);
-struct dir* insert_dir(struct dir* node, char x[250]);
-struct dir* free_dirs(struct dir* node);
-struct dir* remove_dir(struct dir* front, char data[250]);
